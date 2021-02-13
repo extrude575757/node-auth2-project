3b52ea575d8405318751664effb3bfc82f62fb14
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-02-13 02:06:15 -0800

    Basicauth

---
+++ api/auth/rolerestricted-middleware.js
@@ -0,0 +1,13 @@
+module.exports = role => (req, res, next) => {
+
+  // if((req?.decodedJwt?.role  || "") === role){
+  //   next();
+  // }else{
+  //   res.status(403).json({you: "fhd"})
+  // }
+  if (req.decodedJwt && req.decodedJwt.role === role) {
+    next()
+  } else {
+    res.status(403).json('you have no power here')
+  }
+}
